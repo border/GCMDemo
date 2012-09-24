41fe0e7a6873490e6229158291cc523ce25a86ea
Jiang Bian <borderj@gmail.com>
2012-09-24 15:48:40 +0800

    GCM Demo

---
+++ gcm-demo-appengine/run.sh
@@ -0,0 +1,1 @@
+ant -Dsdk.dir=/home/border/work/gae/appengine-java-sdk-1.7.2 runserver -Dserver.host=192.168.1.9
---
+++ gcm-demo-client/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+android update project --name GCMDemo -p . --target android-12
